b477a0bd00aeac645f74422ff58cb73bc12865e6
kimc17 <kimberlycalderonprado@gmail.com>
2020-07-17 12:28:21 -0600

    Se agrega el compilador

---
+++ compilador_cod.py
@@ -0,0 +1,75 @@
+R= {'add' : ["000"], 'mul' : ["001"],'lar' : ["010"], 'and' :["011"],
+       'sub' : ["100"]}
+I= {'ldr' : ["00001"], 'str' :["00010"], 'beq' :["00011"], 'adi' :["00100"], 'bgt' :["00101"] }
+
+
+codigo=""
+f = open("codigo.txt", "r")
+f2 = open("compilado_codigo.data", "w")
+lineas = f.readlines()
+print(lineas)
+Ra= 0
+Rb=0
+b = ",' '"
+for i in lineas:
+    cad = i.strip().lower()
+    print(cad)
+    for char in b:
+        cad = cad.replace(char,"")            
+    
+    if cad.find(":") != -1:
+        print("etiqueta")
+    elif (cad[0:3]  == 'add' or cad[0:3]  == 'mul' or cad[0:3]  == 'lar' or  cad[0:3]  == 'and' or cad[0:3]  == 'sub'):
+        for j in R:
+            if cad[0:3] == j:
+                codigo += "00000"
+                Rd= str(bin(int(cad[4]))[2:])
+                while len(Rd)<4:
+                    Rd ="0"+ Rd
+                codigo += Rd
+                #print(Rd)
+                
+                Ra= str(bin(int(cad[6]))[2:])
+                while len(Ra)<4:
+                    Ra ="0"+ Ra
+                codigo += Ra
+                #print(Ra)
+                
+                Rb= str(bin(int(cad[8]))[2:])
+                while len(Rb)<4:
+                    Rb ="0"+ Rb
+                codigo += Rb
+                codigo += R[j][0]
+                while len(codigo)<32:
+                    codigo =codigo + "0"
+    elif (cad[0:3]  == 'ldr' or cad[0:3]  == 'str' or cad[0:3]  == 'beq' or  cad[0:3]  == 'adi' or cad[0:3]  == 'bgt'):
+        for j in I:
+            if cad[0:3] == j:
+                codigo += I[j][0]
+                            
+                Rd= str(bin(int(cad[4]))[2:])
+                while len(Rd)<4:
+                    Rd ="0"+ Rd
+                codigo += Rd
+                print(Rd)
+                
+                Ra= str(bin(int(cad[6]))[2:])
+                while len(Ra)<4:
+                    Ra ="0"+ Ra
+                codigo += Ra
+                print(Ra)
+                
+                Imm= str(bin(int(cad[8:]))[2:])
+                while len(Imm)<19:
+                    Imm ="0"+ Imm
+                codigo += Imm
+                print(codigo)
+            
+    f2.write( hex(int(codigo[0:4],2))[2:]+ hex(int(codigo[4:8],2))[2:]+
+    hex(int(codigo[8:12],2))[2:]+hex(int(codigo[12:16],2))[2:]+hex(int(codigo[16:20],2))[2:]+
+    hex(int(codigo[20:24],2))[2:]+hex(int(codigo[24:28],2))[2:]+hex(int(codigo[28:32],2))[2:]+'\n')
+
+    codigo = ""
+        
+f.close()
+f2.close()
